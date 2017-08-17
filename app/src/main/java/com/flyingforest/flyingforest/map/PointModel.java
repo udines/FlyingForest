9adabf81f652af6be955f982737b65de35f16fa1
Sendo <sendtofata@live.com>
2017-08-17 13:41:18 +0700

    Add MapActivity and PointModel

---
+++ app/src/main/java/com/flyingforest/flyingforest/map/PointModel.java
@@ -0,0 +1,12 @@
+package com.flyingforest.flyingforest.map;
+
+/**
+ * Created by fata on 8/17/2017.
+ */
+
+public class PointModel {
+    private String id;
+    private String idEvent;
+    private long lat;
+    private long lng;
+}

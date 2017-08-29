c09a3168532d844be21bb535a0a831a9e193835d
Sendo <sendtofata@live.com>
2017-08-29 12:21:09 +0700

    Add image to event item

--- app/src/main/java/com/flyingforest/flyingforest/main/EventViewHolder.java
+++ app/src/main/java/com/flyingforest/flyingforest/main/EventViewHolder.java
@@ -1,5 +1,6 @@
 package com.flyingforest.flyingforest.main;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.flyingforest.flyingforest.R;
 import com.flyingforest.flyingforest.map.MapActivity;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,6 +24,7 @@ public class EventViewHolder extends RecyclerView.ViewHolder {
     private final TextView time;
     private final TextView title;
     private final TextView location;
+    private final RecyclerView recyclerView;
     private final View view;
 
     public EventViewHolder(View itemView) {
@@ -29,6 +32,7 @@ public class EventViewHolder extends RecyclerView.ViewHolder {
         time = (TextView)itemView.findViewById(R.id.item_event_time);
         title = (TextView)itemView.findViewById(R.id.item_event_title);
         location = (TextView)itemView.findViewById(R.id.item_event_location);
+        recyclerView = (RecyclerView)itemView.findViewById(R.id.item_event_recyclerview);
         view = itemView;
     }
 
@@ -36,6 +40,7 @@ public class EventViewHolder extends RecyclerView.ViewHolder {
         title.setText(model.getTitle());
         location.setText(model.getLocation());
         time.setText(getStringDate(model.getTime()));
+        displayImage();
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +51,9 @@ public class EventViewHolder extends RecyclerView.ViewHolder {
                 context.startActivity(mapIntent);
             }
         });
+    }
+
+    private void displayImage() {
     }
 
     private String getStringDate(long time) {

1675a48cb968e433c13844ec97b1deadcdd87560
Fata <sendtofata@live.com>
2017-08-30 21:10:19 +0700

    Add cloud functions to send simple topic notification

---
+++ CloudFunctions/functions/index.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.sendEventNotification = functions.database.ref('/event/{idEvent}').onCreate(event => {
+    const payload = {
+        notification:{
+            title: 'Alert',
+            body: 'Terjadi kebakaran di Yogyakarta',
+            sound: "default"
+        }
+    };
+    return admin.messaging().sendToTopic("notifall", payload);
+});
